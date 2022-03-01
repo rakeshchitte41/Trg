37ba28fb9ffccd0eee499d2eb34c4b0d2fadced3
SriHarshaUppala <100350123+SriHarshaUppala@users.noreply.github.com>
2022-03-01 20:43:22 +0530

    Initial Setup

---
+++ Python CapStone/2022/Graphical Password Authenticator/source/client/Graphical/src/app/graphical/graphical.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-graphical',
+  templateUrl: './graphical.component.html',
+  styleUrls: ['./graphical.component.css'],
+})
+
+
+
+export class GraphicalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
