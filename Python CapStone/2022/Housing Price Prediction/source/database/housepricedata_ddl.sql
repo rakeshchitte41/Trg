b0a897899918d0dd407c93bf4ad8f89aeccdc4af
sudharani nakka <100350143+sudharaninakka@users.noreply.github.com>
2022-03-03 14:50:44 +0530

    database update

---
+++ Python CapStone/2022/Housing Price Prediction/source/database/housepricedata_ddl.sql
@@ -0,0 +1,16 @@
+CREATE TABLE housedb.housepricedataset1 (
+
+  S_no int,
+  Price INT NOT NULL,
+  Area INT NOT NULL,
+  Location VARCHAR(45) NOT NULL,
+  Number_of_bedroom INT NOT NULL,
+  Number_of_bathrooms INT NOT NULL,
+  Density_of_houses_per_unit_area int,
+  Price_per_sqft int,
+  PRIMARY KEY (S_no)
+  );
+  
+  
+
+  
