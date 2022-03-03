67fa04bf1cd06fa074f216ba979a81017aba5328
SriHarshaUppala <100350123+SriHarshaUppala@users.noreply.github.com>
2022-03-03 10:50:49 +0530

    Server add Update

---
+++ Python CapStone/2022/Graphical Password Authenticator/source/server/Graphical-Password-User-Authentincation/home/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import (
+    home_page, 
+    register_page, 
+    login_page, 
+    logout_page, 
+    login_from_uid, 
+    )
+
+urlpatterns = [
+    path('', home_page, name='home'),
+    path('register/', register_page, name='register'),
+    path('login/', login_page, name='login'),
+    path('login/<str:uid>', login_from_uid, name='login_uid'),
+    path('logout/', logout_page, name='logout'),
+]
